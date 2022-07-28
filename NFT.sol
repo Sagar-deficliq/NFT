1d0775e72b0051d08455e11f8aa822421f7550a6
Sagar-deficliq <sagar@deficliq.com>
2022-07-28 10:26:16 +0530

    contract updation 2

--- NFT.sol
+++ NFT.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.11;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -13,14 +12,10 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
  * @title NFT Smart Contract
  * @notice This contract handles minting of ERC721 tokens.
  */
-contract NFT is ERC721A, ReentrancyGuard, Ownable, Pausable {
+contract NFT is ERC721A, ReentrancyGuard, Ownable {
 
     using ECDSA for bytes32;
     using Strings for uint256;
-
-    // Internal vars
-    address account1;
-    address account2;
 
     // Public vars
     string public baseTokenURI;
@@ -47,6 +42,22 @@ contract NFT is ERC721A, ReentrancyGuard, Ownable, Pausable {
         require(maxSupply_ > 0, "INVALID_SUPPLY");
         baseTokenURI = baseTokenURI_;
         maxSupply = maxSupply_;
+    }
+
+    // events to handle pause or unpause for NFT Sale 
+    event Paused();
+    event Unpaused();
+
+    // function to pause NFT Sale
+    function pauseSale() public onlyOwner {
+      isSaleActive = true;
+      emit Paused();
+    }
+
+    // function to unpause NFT Sale
+    function unpauseSale() public onlyOwner {
+      isSaleActive = false;
+      emit Unpaused();
     }
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
@@ -79,20 +90,36 @@ contract NFT is ERC721A, ReentrancyGuard, Ownable, Pausable {
         baseTokenURI = _newBaseURI;
     }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
     function verifyAddressSigner(bytes32 messageHash, bytes memory signature) private view returns (bool) {
         return signerAddress == messageHash.toEthSignedMessageHash().recover(signature);
     }
 
     function hashMessage(address sender, uint256 maximumAllowedMints) private pure returns (bytes32) {
         return keccak256(abi.encode(sender, maximumAllowedMints));
+    }
+
+    /**
+     * @notice Allow for minting of tokens up to the maximum allowed for a given address.
+     * The address of the sender and the number of mints allowed are hashed and signed
+     * with the server's private key and verified here to prove whitelisting status.
+     */
+    function mint(bytes32 messageHash,bytes calldata signature,uint256 mintNumber,uint256 maximumAllowedMints) external payable virtual nonReentrant {
+        require(isSaleActive == false, "NFT :: Sale is not active!!");
+        require(totalSupply() + mintNumber <= maxSupply, "NFT :: Cannot mint beyond max supply!!");
+        require(totalMintsPerAddress[msg.sender] + mintNumber <= maximumAllowedMints, "NFT :: Cannot mint beyond maximum allowed mint!!");
+        require(msg.value >= (price * mintNumber), "NFT :: Payment is below the price!!");
+        require(hashMessage(msg.sender, maximumAllowedMints) == messageHash, "MESSAGE_INVALID");
+        require(verifyAddressSigner(messageHash, signature), "SIGNATURE_VALIDATION_FAILED");
+        // Imprecise floats are scary. Front-end should utilize BigNumber for safe precision, but adding margin just to be safe to not fail txs
+        //require(msg.value >= ((price * mintNumber) - 0.0001 ether) && msg.value <= ((price * mintNumber) + 0.0001 ether), "INVALID_PRICE");
+        
+        totalMintsPerAddress[msg.sender] += mintNumber;
+
+        _safeMint(msg.sender, mintNumber);
+
+        if (totalSupply() + mintNumber >= maxSupply) {
+            isSaleActive = true;
+        }
     }
 
     /**
